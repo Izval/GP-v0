8c2ef6725d9bf9d23592d28d4245974df5f6c190
Izval <izval@protonmail.com>
2024-10-09 13:48:47 -0600

    login2

---
+++ app/pages/index.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+    return (
+      <div className="min-h-screen bg-black flex items-center justify-center">
+        <button className="bg-white text-black px-6 py-3 rounded-lg font-semibold">
+          Login
+        </button>
+      </div>
+    )
+  }
+  
